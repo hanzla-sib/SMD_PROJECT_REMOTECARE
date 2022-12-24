a93e693826be6426978c57802402f40a80f1d554
hanzla sibghat i190515 <hanzlasib@gmail.com>
2022-12-24 20:13:00 +0500

    graph_done

--- app/src/main/java/com/waqasahmad/remotecare/Calories.java
+++ app/src/main/java/com/waqasahmad/remotecare/Calories.java
@@ -16,6 +16,14 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.components.Description;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.formatter.IndexAxisValueFormatter;
+import com.github.mikephil.charting.utils.ColorTemplate;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.jjoe64.graphview.GraphView;
@@ -33,12 +41,14 @@ import java.util.Map;
 
 public class Calories extends AppCompatActivity {
 
-    GraphView graph2;
+    BarChart weekly_barchart,monthly_barchart;
     FirebaseAuth mAuth;
     FirebaseFirestore db;
     String currentemail="";
     JSONArray obj2;
-
+    ArrayList<CaloriesModal> CAL_MODAL=new ArrayList<>();
+    ArrayList<BarEntry> barEntryArrayList;
+    ArrayList<String> Labelsname;
 
     private static final String calorie_graph="http://"+Ip_server.getIpServer()+"/smd_project/calorie_graph.php";
 
@@ -54,9 +64,10 @@ public class Calories extends AppCompatActivity {
 
         //Initializing Firebase MAuth instance
         db = FirebaseFirestore.getInstance();
-        graph2 = findViewById(R.id.graph2);
+        weekly_barchart = findViewById(R.id.graph2);
+        monthly_barchart=findViewById(R.id.graph3);
         //
-         currentemail = mAuth.getCurrentUser().getEmail();
+        currentemail = mAuth.getCurrentUser().getEmail();
 
         StringRequest request=new StringRequest(Request.Method.POST, calorie_graph, new Response.Listener<String>()
         {
@@ -73,31 +84,77 @@ public class Calories extends AppCompatActivity {
                 }
                 else
                 {
+                    Toast.makeText(getApplicationContext(),response,Toast.LENGTH_LONG).show();
+
+                    CAL_MODAL.clear();
+                    barEntryArrayList=new ArrayList<>();
+                    Labelsname=new ArrayList<>();
+                    barEntryArrayList.clear();
+                    Labelsname.clear();
+
+
 
 
                     try {
                         obj2 = new JSONArray(response);
-//                        DataPoint[] dp = new DataPoint[obj2.length()];
-//                        for(int i=1;i<obj2.length();i++){
-//                            JSONObject jsonObject = obj2.getJSONObject(i);
-//                            String date = jsonObject.getString("date");
-//                            String Calorie = jsonObject.getString("calorie");
-//
-//                            dp[i] = new DataPoint(10, 10);
-//                        }
-                        DataPoint[] dp = new DataPoint[10];
-                        for(int i=1;i<=10;i++){
-                            dp[i] = new DataPoint(1, 2);
+
+                        for(int i=1;i<obj2.length();i++){
+                            JSONObject jsonObject = obj2.getJSONObject(i);
+                            String date = jsonObject.getString("date");
+                            String halfdate="";
+                            for(int j=5;j<date.length();j++){
+                               halfdate+=date.charAt(j);
+                            }
+                            String Calorie = jsonObject.getString("calorie");
+                            CAL_MODAL.add(new CaloriesModal(halfdate,Integer.parseInt(Calorie)));
+//                            CAL_MODAL.add(new CaloriesModal(date,Integer.parseInt(Calorie)));
                         }
 
-                        BarGraphSeries<DataPoint> series = new BarGraphSeries<>(dp);
-                        graph2.addSeries(series);
+                        for(int i=0;i<CAL_MODAL.size();i++){
+                            String month=CAL_MODAL.get(i).getDate();
+                            int Cal=CAL_MODAL.get(i).getCalories();
+                            barEntryArrayList.add(new BarEntry(i,Cal));
+                            Labelsname.add(month);
+                        }
+                        BarDataSet barDataSetweekly=new BarDataSet(barEntryArrayList,"Weekly CALORIES");
+                        barDataSetweekly.setColors(ColorTemplate.COLORFUL_COLORS);
+                        Description description_weekly= new Description();
+                        description_weekly.setText("-");
+                        weekly_barchart.setDescription(description_weekly);
+                        BarData barData_weekly=new BarData((barDataSetweekly));
+                        weekly_barchart.setData(barData_weekly);
+                        XAxis xAxis_weekly=weekly_barchart.getXAxis();
+                        xAxis_weekly.setValueFormatter(new IndexAxisValueFormatter(Labelsname));
+                        xAxis_weekly.setPosition(XAxis.XAxisPosition.BOTTOM);
+                        xAxis_weekly.setDrawGridLines(false);
+                        xAxis_weekly.setDrawAxisLine(false);
+                        xAxis_weekly.setGranularity(1f);
+                        xAxis_weekly.setLabelCount(Labelsname.size());
+                        xAxis_weekly.setLabelRotationAngle(0);
+                        weekly_barchart.animateY(2000);
+                        weekly_barchart.invalidate();
+
+                        //====================
+//                        monthly
+                        BarDataSet barDataSet_monthly=new BarDataSet(barEntryArrayList,"Monthly CALORIES");
+                        barDataSet_monthly.setColors(ColorTemplate.COLORFUL_COLORS);
+                        Description description= new Description();
+                        description.setText("-");
+                        monthly_barchart.setDescription(description);
+                        BarData barData_monthly=new BarData((barDataSet_monthly));
+                        monthly_barchart.setData(barData_monthly);
+                        XAxis xAxis=monthly_barchart.getXAxis();
+                        xAxis.setValueFormatter(new IndexAxisValueFormatter(Labelsname));
+                        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
+                        xAxis.setDrawGridLines(false);
+                        xAxis.setDrawAxisLine(false);
+                        xAxis.setGranularity(1f);
+                        xAxis.setLabelCount(Labelsname.size());
+                        xAxis.setLabelRotationAngle(0);
+                        monthly_barchart.animateY(2000);
+                        monthly_barchart.invalidate();
                     } catch (JSONException e) {
                         e.printStackTrace();
-
-
-
-
                     }
 
                 }
---
+++ app/src/main/java/com/waqasahmad/remotecare/CaloriesModal.java
@@ -0,0 +1,27 @@
+package com.waqasahmad.remotecare;
+public class CaloriesModal {
+    String date;
+
+    public CaloriesModal(String date, int calories) {
+        this.date = date;
+        Calories = calories;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public int getCalories() {
+        return Calories;
+    }
+
+    public void setCalories(int calories) {
+        Calories = calories;
+    }
+
+    int Calories;
+}
