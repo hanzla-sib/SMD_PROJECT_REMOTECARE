71c60b1d33b34c264d22315dbb5c64bf08aa8e9b
hanzla sibghat i190515 <hanzlasib@gmail.com>
2023-01-08 16:03:26 +0500

    phpfiles

--- smd_project/inserting dummy table.sql
+++ smd_project/inserting dummy table.sql
@@ -1,34 +1,123 @@
-INSERT INTO `consumed_calories`( `p_email`, `date_log`, `Calories`) VALUES ('u@gmail.com','2022-12-20','1000');
-INSERT INTO `consumed_calories`( `p_email`, `date_log`, `Calories`) VALUES ('u@gmail.com','2022-12-21','3000');
-INSERT INTO `consumed_calories`( `p_email`, `date_log`, `Calories`) VALUES ('u@gmail.com','2022-12-22','1500');
-INSERT INTO `consumed_calories`( `p_email`, `date_log`, `Calories`) VALUES ('u@gmail.com','2022-12-23','2000');
-INSERT INTO `consumed_calories`( `p_email`, `date_log`, `Calories`) VALUES ('u@gmail.com','2022-12-24','500');
-INSERT INTO `consumed_calories`( `p_email`, `date_log`, `Calories`) VALUES ('u@gmail.com','2022-12-25','900');
-INSERT INTO `consumed_calories`( `p_email`, `date_log`, `Calories`) VALUES ('u@gmail.com','2022-12-26','1800');
-INSERT INTO `consumed_calories`( `p_email`, `date_log`, `Calories`) VALUES ('u@gmail.com','2022-12-27','2022');
-INSERT INTO `consumed_calories`( `p_email`, `date_log`, `Calories`) VALUES ('u@gmail.com','2022-12-28','2200');
-INSERT INTO `consumed_calories`( `p_email`, `date_log`, `Calories`) VALUES ('u@gmail.com','2022-12-29','1200');
-INSERT INTO `consumed_calories`( `p_email`, `date_log`, `Calories`) VALUES ('u@gmail.com','2022-12-30','1700');
-INSERT INTO `consumed_calories`( `p_email`, `date_log`, `Calories`) VALUES ('u@gmail.com','2022-12-31','1900');
-INSERT INTO `consumed_calories`( `p_email`, `date_log`, `Calories`) VALUES ('u@gmail.com','2023-01-01','1000');
-INSERT INTO `consumed_calories`( `p_email`, `date_log`, `Calories`) VALUES ('u@gmail.com','2023-01-02','550');
-INSERT INTO `consumed_calories`( `p_email`, `date_log`, `Calories`) VALUES ('u@gmail.com','2023-01-03','900');
-INSERT INTO `consumed_calories`( `p_email`, `date_log`, `Calories`) VALUES ('u@gmail.com','2023-01-04','1100');
+
+INSERT INTO `user`(`name`, `email`, `password`, `user_type`, `gender`, `imageurl`) VALUES ('Umaid Khakwani','umaid@gmail.com','123456','1','Male','07-01-2023-1673128609-54295.jpg');
+INSERT INTO `user`(`name`, `email`, `password`, `user_type`, `gender`, `imageurl`) VALUES ('Hanzla Sibghat','hanzla@gmail.com','123456','1','Male','07-01-2023-1673128736-93895.jpg');
+INSERT INTO `user`(`name`, `email`, `password`, `user_type`, `gender`, `imageurl`) VALUES ('Waqas Ahmad','waqas@gmail.com','123456','2','Male','07-01-2023-1673128814-20872.jpg');
+INSERT INTO `user`(`name`, `email`, `password`, `user_type`, `gender`, `imageurl`) VALUES ('Taha','taha@gmail.com','123456','2','Male','07-01-2023-1673128879-59766.jpg');
+INSERT INTO `user`(`name`, `email`, `password`, `user_type`, `gender`, `imageurl`) VALUES ('Ahmad Arif','ahmad@gmail.com','123456','2','Male','07-01-2023-1673128958-92729.jpg');
 
 
-INSERT INTO `weekly_steps`( `Demail`,`steps_daily`, `date_log`, `Burnt_Calories`) VALUES ('u@gmail.com','500','2022-12-20','500');
-INSERT INTO `weekly_steps`( `Demail`,`steps_daily`, `date_log`, `Burnt_Calories`) VALUES ('u@gmail.com','1000','2022-12-21','200');
-INSERT INTO `weekly_steps`( `Demail`,`steps_daily`, `date_log`, `Burnt_Calories`) VALUES ('u@gmail.com','3000','2022-12-22','550');
-INSERT INTO `weekly_steps`( `Demail`,`steps_daily`, `date_log`, `Burnt_Calories`) VALUES ('u@gmail.com','2000','2022-12-23','600');
-INSERT INTO `weekly_steps`( `Demail`,`steps_daily`, `date_log`, `Burnt_Calories`) VALUES ('u@gmail.com','1500','2022-12-24','670');
-INSERT INTO `weekly_steps`( `Demail`,`steps_daily`, `date_log`, `Burnt_Calories`) VALUES ('u@gmail.com','1100','2022-12-25','679');
-INSERT INTO `weekly_steps`( `Demail`,`steps_daily`, `date_log`, `Burnt_Calories`) VALUES ('u@gmail.com','1400','2022-12-26','170');
-INSERT INTO `weekly_steps`( `Demail`,`steps_daily`, `date_log`, `Burnt_Calories`) VALUES ('u@gmail.com','1900','2022-12-27','800');
-INSERT INTO `weekly_steps`( `Demail`,`steps_daily`, `date_log`, `Burnt_Calories`) VALUES ('u@gmail.com','1800','2022-12-28','1000');
-INSERT INTO `weekly_steps`( `Demail`,`steps_daily`, `date_log`, `Burnt_Calories`) VALUES ('u@gmail.com','2200','2022-12-29','1400');
-INSERT INTO `weekly_steps`( `Demail`,`steps_daily`, `date_log`, `Burnt_Calories`) VALUES ('u@gmail.com','2400','2022-12-30','1000');
-INSERT INTO `weekly_steps`( `Demail`,`steps_daily`, `date_log`, `Burnt_Calories`) VALUES ('u@gmail.com','2700','2022-12-31','2300');
-INSERT INTO `weekly_steps`( `Demail`,`steps_daily`, `date_log`, `Burnt_Calories`) VALUES ('u@gmail.com','3000','2023-01-01','2300');
-INSERT INTO `weekly_steps`( `Demail`,`steps_daily`, `date_log`, `Burnt_Calories`) VALUES ('u@gmail.com','3200','2023-01-02','2400');
-INSERT INTO `weekly_steps`( `Demail`,`steps_daily`, `date_log`, `Burnt_Calories`) VALUES ('u@gmail.com','400','2023-01-03','900');
-INSERT INTO `weekly_steps`( `Demail`,`steps_daily`, `date_log`, `Burnt_Calories`) VALUES ('u@gmail.com','1000','2023-01-04','1100');
+
+
+INSERT INTO `test_record`(`Temail`, `imageurl`, `details`) VALUES ('umaid@gmail.com','07-01-2023-1673128546-48021.jpg','sale');
+INSERT INTO `test_record`(`Temail`, `imageurl`, `details`) VALUES ('umaid@gmail.com','07-01-2023-1673128574-92854.jpg','internship');
+INSERT INTO `test_record`(`Temail`, `imageurl`, `details`) VALUES ('umaid@gmail.com','07-01-2023-1673128594-44076.jpg','mosam');
+INSERT INTO `test_record`(`Temail`, `imageurl`, `details`) VALUES ('hanzla@gmail.com','07-01-2023-1673128683-34068.jpg','calender');
+INSERT INTO `test_record`(`Temail`, `imageurl`, `details`) VALUES ('hanzla@gmail.com','07-01-2023-1673128712-59094.jpg','specs');
+INSERT INTO `test_record`(`Temail`, `imageurl`, `details`) VALUES ('hanzla@gmail.com','07-01-2023-1673128728-21843.jpg','jersey');
+
+
+INSERT INTO `consumed_calories`( `p_email`, `date_log`, `Calories`) VALUES ('umaid@gmail.com','2022-12-20','1000');
+INSERT INTO `consumed_calories`( `p_email`, `date_log`, `Calories`) VALUES ('umaid@gmail.com','2022-12-21','3000');
+INSERT INTO `consumed_calories`( `p_email`, `date_log`, `Calories`) VALUES ('umaid@gmail.com','2022-12-22','1500');
+INSERT INTO `consumed_calories`( `p_email`, `date_log`, `Calories`) VALUES ('umaid@gmail.com','2022-12-23','2000');
+INSERT INTO `consumed_calories`( `p_email`, `date_log`, `Calories`) VALUES ('umaid@gmail.com','2022-12-24','500');
+INSERT INTO `consumed_calories`( `p_email`, `date_log`, `Calories`) VALUES ('umaid@gmail.com','2022-12-25','900');
+INSERT INTO `consumed_calories`( `p_email`, `date_log`, `Calories`) VALUES ('umaid@gmail.com','2022-12-26','1800');
+INSERT INTO `consumed_calories`( `p_email`, `date_log`, `Calories`) VALUES ('umaid@gmail.com','2022-12-27','2022');
+INSERT INTO `consumed_calories`( `p_email`, `date_log`, `Calories`) VALUES ('umaid@gmail.com','2022-12-28','2200');
+INSERT INTO `consumed_calories`( `p_email`, `date_log`, `Calories`) VALUES ('umaid@gmail.com','2022-12-29','1200');
+INSERT INTO `consumed_calories`( `p_email`, `date_log`, `Calories`) VALUES ('umaid@gmail.com','2022-12-30','1700');
+INSERT INTO `consumed_calories`( `p_email`, `date_log`, `Calories`) VALUES ('umaid@gmail.com','2022-12-31','1900');
+INSERT INTO `consumed_calories`( `p_email`, `date_log`, `Calories`) VALUES ('umaid@gmail.com','2023-01-01','1000');
+INSERT INTO `consumed_calories`( `p_email`, `date_log`, `Calories`) VALUES ('umaid@gmail.com','2023-01-02','550');
+INSERT INTO `consumed_calories`( `p_email`, `date_log`, `Calories`) VALUES ('umaid@gmail.com','2023-01-03','900');
+INSERT INTO `consumed_calories`( `p_email`, `date_log`, `Calories`) VALUES ('umaid@gmail.com','2023-01-04','1100');
+
+INSERT INTO `consumed_calories`( `p_email`, `date_log`, `Calories`) VALUES ('umaid@gmail.com','2023-01-05','600');
+INSERT INTO `consumed_calories`( `p_email`, `date_log`, `Calories`) VALUES ('umaid@gmail.com','2023-01-06','1100');
+INSERT INTO `consumed_calories`( `p_email`, `date_log`, `Calories`) VALUES ('umaid@gmail.com','2023-01-07','1300');
+INSERT INTO `consumed_calories`( `p_email`, `date_log`, `Calories`) VALUES ('umaid@gmail.com','2023-01-08','1500');
+
+
+INSERT INTO `weekly_steps`( `Demail`,`steps_daily`, `date_log`, `Burnt_Calories`) VALUES ('umaid@gmail.com','500','2022-12-20','500');
+INSERT INTO `weekly_steps`( `Demail`,`steps_daily`, `date_log`, `Burnt_Calories`) VALUES ('umaid@gmail.com','1000','2022-12-21','200');
+INSERT INTO `weekly_steps`( `Demail`,`steps_daily`, `date_log`, `Burnt_Calories`) VALUES ('umaid@gmail.com','3000','2022-12-22','550');
+INSERT INTO `weekly_steps`( `Demail`,`steps_daily`, `date_log`, `Burnt_Calories`) VALUES ('umaid@gmail.com','2000','2022-12-23','600');
+INSERT INTO `weekly_steps`( `Demail`,`steps_daily`, `date_log`, `Burnt_Calories`) VALUES ('umaid@gmail.com','1500','2022-12-24','670');
+INSERT INTO `weekly_steps`( `Demail`,`steps_daily`, `date_log`, `Burnt_Calories`) VALUES ('umaid@gmail.com','1100','2022-12-25','679');
+INSERT INTO `weekly_steps`( `Demail`,`steps_daily`, `date_log`, `Burnt_Calories`) VALUES ('umaid@gmail.com','1400','2022-12-26','170');
+INSERT INTO `weekly_steps`( `Demail`,`steps_daily`, `date_log`, `Burnt_Calories`) VALUES ('umaid@gmail.com','1900','2022-12-27','800');
+INSERT INTO `weekly_steps`( `Demail`,`steps_daily`, `date_log`, `Burnt_Calories`) VALUES ('umaid@gmail.com','1800','2022-12-28','1000');
+INSERT INTO `weekly_steps`( `Demail`,`steps_daily`, `date_log`, `Burnt_Calories`) VALUES ('umaid@gmail.com','2200','2022-12-29','1400');
+INSERT INTO `weekly_steps`( `Demail`,`steps_daily`, `date_log`, `Burnt_Calories`) VALUES ('umaid@gmail.com','2400','2022-12-30','1000');
+INSERT INTO `weekly_steps`( `Demail`,`steps_daily`, `date_log`, `Burnt_Calories`) VALUES ('umaid@gmail.com','2700','2022-12-31','2300');
+INSERT INTO `weekly_steps`( `Demail`,`steps_daily`, `date_log`, `Burnt_Calories`) VALUES ('umaid@gmail.com','3000','2023-01-01','2300');
+INSERT INTO `weekly_steps`( `Demail`,`steps_daily`, `date_log`, `Burnt_Calories`) VALUES ('umaid@gmail.com','3200','2023-01-02','2400');
+INSERT INTO `weekly_steps`( `Demail`,`steps_daily`, `date_log`, `Burnt_Calories`) VALUES ('umaid@gmail.com','400','2023-01-03','900');
+INSERT INTO `weekly_steps`( `Demail`,`steps_daily`, `date_log`, `Burnt_Calories`) VALUES ('umaid@gmail.com','1000','2023-01-04','1100');
+INSERT INTO `weekly_steps`( `Demail`,`steps_daily`, `date_log`, `Burnt_Calories`) VALUES ('umaid@gmail.com','3000','2023-01-05','2300');
+INSERT INTO `weekly_steps`( `Demail`,`steps_daily`, `date_log`, `Burnt_Calories`) VALUES ('umaid@gmail.com','3200','2023-01-06','2400');
+INSERT INTO `weekly_steps`( `Demail`,`steps_daily`, `date_log`, `Burnt_Calories`) VALUES ('umaid@gmail.com','400','2023-01-07','900');
+INSERT INTO `weekly_steps`( `Demail`,`steps_daily`, `date_log`, `Burnt_Calories`) VALUES ('umaid@gmail.com','1000','2023-01-08','1100');
+
+
+
+INSERT INTO `consumed_calories`( `p_email`, `date_log`, `Calories`) VALUES ('hanzla@gmail.com','2022-12-20','1000');
+INSERT INTO `consumed_calories`( `p_email`, `date_log`, `Calories`) VALUES ('hanzla@gmail.com','2022-12-21','3000');
+INSERT INTO `consumed_calories`( `p_email`, `date_log`, `Calories`) VALUES ('hanzla@gmail.com','2022-12-22','1500');
+INSERT INTO `consumed_calories`( `p_email`, `date_log`, `Calories`) VALUES ('hanzla@gmail.com','2022-12-23','2000');
+INSERT INTO `consumed_calories`( `p_email`, `date_log`, `Calories`) VALUES ('hanzla@gmail.com','2022-12-24','500');
+INSERT INTO `consumed_calories`( `p_email`, `date_log`, `Calories`) VALUES ('hanzla@gmail.com','2022-12-25','900');
+INSERT INTO `consumed_calories`( `p_email`, `date_log`, `Calories`) VALUES ('hanzla@gmail.com','2022-12-26','1800');
+INSERT INTO `consumed_calories`( `p_email`, `date_log`, `Calories`) VALUES ('hanzla@gmail.com','2022-12-27','2022');
+INSERT INTO `consumed_calories`( `p_email`, `date_log`, `Calories`) VALUES ('hanzla@gmail.com','2022-12-28','2200');
+INSERT INTO `consumed_calories`( `p_email`, `date_log`, `Calories`) VALUES ('hanzla@gmail.com','2022-12-29','1200');
+INSERT INTO `consumed_calories`( `p_email`, `date_log`, `Calories`) VALUES ('hanzla@gmail.com','2022-12-30','1700');
+INSERT INTO `consumed_calories`( `p_email`, `date_log`, `Calories`) VALUES ('hanzla@gmail.com','2022-12-31','1900');
+INSERT INTO `consumed_calories`( `p_email`, `date_log`, `Calories`) VALUES ('hanzla@gmail.com','2023-01-01','1000');
+INSERT INTO `consumed_calories`( `p_email`, `date_log`, `Calories`) VALUES ('hanzla@gmail.com','2023-01-02','550');
+INSERT INTO `consumed_calories`( `p_email`, `date_log`, `Calories`) VALUES ('hanzla@gmail.com','2023-01-03','900');
+INSERT INTO `consumed_calories`( `p_email`, `date_log`, `Calories`) VALUES ('hanzla@gmail.com','2023-01-04','1100');
+INSERT INTO `consumed_calories`( `p_email`, `date_log`, `Calories`) VALUES ('hanzla@gmail.com','2023-01-05','1000');
+INSERT INTO `consumed_calories`( `p_email`, `date_log`, `Calories`) VALUES ('hanzla@gmail.com','2023-01-06','550');
+INSERT INTO `consumed_calories`( `p_email`, `date_log`, `Calories`) VALUES ('hanzla@gmail.com','2023-01-07','900');
+INSERT INTO `consumed_calories`( `p_email`, `date_log`, `Calories`) VALUES ('hanzla@gmail.com','2023-01-08','1100');
+
+
+INSERT INTO `weekly_steps`( `Demail`,`steps_daily`, `date_log`, `Burnt_Calories`) VALUES ('hanzla@gmail.com','500','2022-12-20','500');
+INSERT INTO `weekly_steps`( `Demail`,`steps_daily`, `date_log`, `Burnt_Calories`) VALUES ('hanzla@gmail.com','1000','2022-12-21','200');
+INSERT INTO `weekly_steps`( `Demail`,`steps_daily`, `date_log`, `Burnt_Calories`) VALUES ('hanzla@gmail.com','3000','2022-12-22','550');
+INSERT INTO `weekly_steps`( `Demail`,`steps_daily`, `date_log`, `Burnt_Calories`) VALUES ('hanzla@gmail.com','2000','2022-12-23','600');
+INSERT INTO `weekly_steps`( `Demail`,`steps_daily`, `date_log`, `Burnt_Calories`) VALUES ('hanzla@gmail.com','1500','2022-12-24','670');
+INSERT INTO `weekly_steps`( `Demail`,`steps_daily`, `date_log`, `Burnt_Calories`) VALUES ('hanzla@gmail.com','1100','2022-12-25','679');
+INSERT INTO `weekly_steps`( `Demail`,`steps_daily`, `date_log`, `Burnt_Calories`) VALUES ('hanzla@gmail.com','1400','2022-12-26','170');
+INSERT INTO `weekly_steps`( `Demail`,`steps_daily`, `date_log`, `Burnt_Calories`) VALUES ('hanzla@gmail.com','1900','2022-12-27','800');
+INSERT INTO `weekly_steps`( `Demail`,`steps_daily`, `date_log`, `Burnt_Calories`) VALUES ('hanzla@gmail.com','1800','2022-12-28','1000');
+INSERT INTO `weekly_steps`( `Demail`,`steps_daily`, `date_log`, `Burnt_Calories`) VALUES ('hanzla@gmail.com','2200','2022-12-29','1400');
+INSERT INTO `weekly_steps`( `Demail`,`steps_daily`, `date_log`, `Burnt_Calories`) VALUES ('hanzla@gmail.com','2400','2022-12-30','1000');
+INSERT INTO `weekly_steps`( `Demail`,`steps_daily`, `date_log`, `Burnt_Calories`) VALUES ('hanzla@gmail.com','2700','2022-12-31','2300');
+INSERT INTO `weekly_steps`( `Demail`,`steps_daily`, `date_log`, `Burnt_Calories`) VALUES ('hanzla@gmail.com','3000','2023-01-01','2300');
+INSERT INTO `weekly_steps`( `Demail`,`steps_daily`, `date_log`, `Burnt_Calories`) VALUES ('hanzla@gmail.com','3200','2023-01-02','2400');
+INSERT INTO `weekly_steps`( `Demail`,`steps_daily`, `date_log`, `Burnt_Calories`) VALUES ('hanzla@gmail.com','400','2023-01-03','900');
+INSERT INTO `weekly_steps`( `Demail`,`steps_daily`, `date_log`, `Burnt_Calories`) VALUES ('hanzla@gmail.com','1000','2023-01-04','1100');
+INSERT INTO `weekly_steps`( `Demail`,`steps_daily`, `date_log`, `Burnt_Calories`) VALUES ('hanzla@gmail.com','3000','2023-01-05','2300');
+INSERT INTO `weekly_steps`( `Demail`,`steps_daily`, `date_log`, `Burnt_Calories`) VALUES ('hanzla@gmail.com','3200','2023-01-06','2400');
+INSERT INTO `weekly_steps`( `Demail`,`steps_daily`, `date_log`, `Burnt_Calories`) VALUES ('hanzla@gmail.com','400','2023-01-07','900');
+INSERT INTO `weekly_steps`( `Demail`,`steps_daily`, `date_log`, `Burnt_Calories`) VALUES ('hanzla@gmail.com','1000','2023-01-08','1100');
+
+
+
+INSERT INTO `daily_steps`( `Demail`, `steps_daily`, `date_log`, `motion`, `Burnt_Calories`) VALUES ('umaid@gmail.com','20','2023-01-08','Resting','0.8');
+INSERT INTO `daily_steps`( `Demail`, `steps_daily`, `date_log`, `motion`, `Burnt_Calories`) VALUES ('hanzla@gmail.com','20','2023-01-08','Resting','0.8');
+
+
+INSERT INTO `patient_appointment`(`p_name`, `p_email`, `appoint_status`, `Time1`, `Date1`, `d_name`, `d_email`) VALUES ('Umaid Khakwani','umaid@gmail.com','Accepted','6:40','13-1-2023','Taha','taha@gmail.com');
+INSERT INTO `patient_appointment`(`p_name`, `p_email`, `appoint_status`, `d_name`, `d_email`) VALUES ('Umaid Khakwani','umaid@gmail.com','Pending','Waqas Ahmad','waqas@gmail.com');
+INSERT INTO `patient_appointment`(`p_name`, `p_email`, `appoint_status`,`d_name`, `d_email`) VALUES ('Hanzla Sibghat','hanzla@gmail.com','Pending','Ahmad Arif','ahmad@gmail.com');
+INSERT INTO `patient_appointment`(`p_name`, `p_email`, `appoint_status`, `Time1`, `Date1`, `d_name`, `d_email`) VALUES ('Hanzla Sibghat','hanzla@gmail.com','Accepted','9:40','13-1-2023','Taha','taha@gmail.com');
+
+
+INSERT INTO `doctor_appointment`(`d_name`, `d_email`, `appoint_status`, `p_name`, `p_email`) VALUES ('Ahmad Arif','ahmad@gmail.com','Pending','Hanzla Sibghat','hanzla@gmail.com');
+INSERT INTO `doctor_appointment`(`d_name`, `d_email`, `appoint_status`, `Time1`, `Date1`, `p_name`, `p_email`) VALUES ('Taha','taha@gmail.com','Accepted','9:40','13-1-2023','Hanzla Sibghat','hanzla@gmail.com');
+INSERT INTO `doctor_appointment`(`d_name`, `d_email`, `appoint_status`, `Time1`, `Date1`, `p_name`, `p_email`) VALUES ('Taha','taha@gmail.com','Accepted','6:40','13-1-2023','Umaid Khakwani','umaid@gmail.com');
+INSERT INTO `doctor_appointment`(`d_name`, `d_email`, `appoint_status`, `p_name`, `p_email`) VALUES ('Waqas Ahmad','waqas@gmail.com','Pending','Umaid Khakwani','umaid@gmail.com');
