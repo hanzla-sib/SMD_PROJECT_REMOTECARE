59eeefd6789425b6664d5cf6930caf83b85cc163
hanzla sibghat i190515 <hanzlasib@gmail.com>
2023-05-14 15:57:43 +0500

    gpt_try

--- app/src/main/java/com/waqasahmad/remotecare/Gpt_patient_side.java
+++ app/src/main/java/com/waqasahmad/remotecare/Gpt_patient_side.java
@@ -1,17 +1,40 @@
 package com.waqasahmad.remotecare;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.ScrollView;
+import android.widget.TextView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.util.Collections;
+
+import java.util.Collections;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 public class Gpt_patient_side extends AppCompatActivity {
+    public static final MediaType JSON=MediaType.get("application/json; charset=utf-8");
+    OkHttpClient client=new OkHttpClient();
 
+    private static final String[] STOP = new String[] { "." };
     LinearLayout back_btn;
     LinearLayout btn1, btn2, btn3, btn4;
     EditText query;
@@ -35,6 +58,18 @@ public class Gpt_patient_side extends AppCompatActivity {
         scroll_view_answer =findViewById(R.id.scroll_view_answer);
 
 
+        submit_query_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String prompt = query.getText().toString();
+                Log.d("promot", prompt);
+                try {
+                    callApi(prompt);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
 
 
 
@@ -72,4 +107,49 @@ public class Gpt_patient_side extends AppCompatActivity {
 
 
     }
+
+    void callApi(String question) throws JSONException {
+        Log.d("prompt",question);
+        JSONObject jsonBody=new JSONObject();
+
+        jsonBody.put("model","text-davinci-003");
+        jsonBody.put("prompt",question);
+        jsonBody.put("max_tokens",4000);
+        jsonBody.put("temperature",0);
+
+        RequestBody body= RequestBody.create(jsonBody.toString(),JSON);
+        Request request= new Request.Builder().url("https://api.openai.com/v1/completions")
+                .header("Authorization","Bearer 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")
+                .post(body)
+                .build();
+
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(@NonNull Call call, @NonNull IOException e) {
+            Log.d("promot","failure");
+            }
+
+            @Override
+            public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
+
+
+                try {
+                    JSONObject jsonObject = new JSONObject(response.body().string());
+                    Log.d("hello object",jsonObject.toString());
+                    if (jsonObject.has("choices")) {
+                        JSONArray jsonArray = jsonObject.getJSONArray("choices");
+                        String result = jsonArray.getJSONObject(0).getString("text");
+                        Log.d("hello ,", result.trim());
+                    } else {
+                        Log.d("response", "No 'choices' key in JSON object");
+                    }
+                } catch (JSONException e) {
+                    Log.d("response", "Error parsing JSON object: " + e.getMessage());
+                    e.printStackTrace();
+                }
+
+            }
+        });
+    }
+
 }
